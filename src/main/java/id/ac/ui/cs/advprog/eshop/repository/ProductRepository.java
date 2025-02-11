da8a20997a7e20741ffe232f9d3009655a84bb58
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2025-02-11 19:40:48 +0700

    feat: add delete method to ProductRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,19 +1,25 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
-import  org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public class ProductRepository {
     private List<Product> productData = new ArrayList<>();
 
     public Product create(Product product) {
+        product.setProductId(UUID.randomUUID().toString());
         productData.add(product);
         return product;
+    }
+
+    public boolean delete(String productId) {
+        return productData.removeIf(product -> product.getProductId().equals(productId));
     }
 
     public Iterator<Product> findAll() {
