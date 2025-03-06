a74d297552311b66878490588845606841ab25cf
Malvin Scafi <154596305+Cyades@users.noreply.github.com>
2025-03-07 05:29:54 +0700

    [GREEN] Implement Payment model.

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -8,23 +8,40 @@ import java.util.UUID;
 public class Payment {
     String id;
     String method;
-    String status;
     Map<String, String> paymentData;
     Order order;
 
     public Payment(String id, Order order, String method, Map<String, String> paymentData) {
-        // Constructor implementation
+        this(order, method, paymentData);
+        this.id = id;
     }
 
     public Payment(Order order, String method, Map<String, String> paymentData) {
-        // Constructor implementation
-    }
-
-    void setStatus(String status) {
-        // Method implementation
+        this.id = UUID.randomUUID().toString();
+        this.method = method;
+        this.order = order;
+        if (paymentData == null) {
+            throw new IllegalArgumentException("Payment data is null");
+        }
+        setPaymentData(paymentData);
     }
 
     public void setPaymentData(Map<String, String> paymentData) {
-        // Method implementation
+        if (method.equals("BANK")) {
+            if (paymentData.get("bankName").isBlank() || paymentData.get("referenceCode").isBlank()) {
+                throw new IllegalArgumentException("Bank name or reference code is blank");
+            }
+        } else if (method.equals("VOUCHER")) {
+            int count = 0;
+            for (int i = 0; i < paymentData.get("voucher").length(); i++) {
+                if (Character.isDigit(paymentData.get("voucher").charAt(i))) {
+                    count++;
+                }
+            }
+            if (paymentData.get("voucher").length() != 16 || !paymentData.get("voucher").startsWith("ESHOP") || count != 8) {
+                throw new IllegalArgumentException("Voucher not supported");
+            }
+        }
+        this.paymentData = paymentData;
     }
 }
